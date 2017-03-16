cf100ec818d5a47f79e402415875afd4f6fe9b6d
DELL <DELL@DESKTOP-MC8LGET>
2017-03-16 21:48:00 +0500

    condition opertor

---
+++ JavaApplication8/src/Sample2.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+public class Sample2 {
+    public static void main(String[]args)
+    {
+    
+    Scanner sc=new Scanner(System.in);
+    int a,b,c;
+    System.out.println("enter the value of a=");
+    a=sc.nextInt();
+    
+    System.out.println("enter the value of b=");
+    b=sc.nextInt();
+    
+    c=(a>b)?a:b; //agr condition thq hoe tw a ki value dkhay ga wrna b ki 
+                 //if else wala kam ha
+    
+    
+    System.out.println("the value of c is"+c);
+    }
+}
+    
+    
+    
+    
+    
+    
+    
+    }
+    
+    
+    
+}
