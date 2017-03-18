faf20666177bc6001c1ebf40044f52a7ecc51381
DELL <DELL@DESKTOP-MC8LGET>
2017-03-18 21:15:17 +0500

    simple program

---
+++ JavaApplication8/src/Class.java
@@ -0,0 +1,16 @@
+
+public class Class {
+    public static void main(String[]args)
+    {
+    int a;
+    a=10;
+    
+    System.out.println("the result is \t"+a);
+    
+    
+    
+    
+    
+    }
+    
+}
