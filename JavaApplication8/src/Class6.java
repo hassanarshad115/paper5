91b1aacb7ec7e06627f3ce3ee0fe2727362c8ec6
DELL <DELL@DESKTOP-MC8LGET>
2017-03-18 21:27:35 +0500

    postfix and prefix

---
+++ JavaApplication8/src/Class6.java
@@ -0,0 +1,25 @@
+class Class6 
+{
+public static void main(String [] args)
+{
+int a=5;
+int b=5;
+
+//postfix
+a++;
+System.out.println(a );
+
+b=a++ ;
+System.out.println(b );
+System.out.println();
+
+//postfix
+++a;
+System.out.println(a );
+
+b=++a ;
+System.out.println(b );
+System.out.println();
+}
+
+}
