a77e0c46c6c7589cc3c9baf220ce309034dcc19e
DELL <DELL@DESKTOP-MC8LGET>
2017-03-18 21:40:15 +0500

    all assigment programs

---
+++ JavaApplication8/src/Sample1.java
@@ -0,0 +1,28 @@
+//all boolean 
+public class Sample1 {
+    public static void main(String[]args)
+    {
+    boolean b;
+    
+    //b=(5>1);true
+    //b=(5<1);false
+    //b=(1==1);true
+   // b=(1==1)&& (5>1);true
+      //  b=(1==1)&& (5<1);false
+              //b=(1==1)|| (5<1);true
+                     //b=(1==1)|| (5>1);ture
+             // b=(1!=1)|| (5<1);false
+             // b=(0!=1)|| (5<1);true
+
+
+
+
+    
+    
+    System.out.println(b);
+    
+    
+    
+    
+    }
+}
