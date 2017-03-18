e0cc673a72fba4c8c4818c633befab3a1da07150
DELL <DELL@DESKTOP-MC8LGET>
2017-03-18 21:26:46 +0500

    string contains

---
+++ JavaApplication8/src/Class5.java
@@ -0,0 +1,14 @@
+public class Class5 {
+    public static void main(String[]args)
+    {
+        
+    
+ String s1="hi am hassan am from bahawalnagar";
+    boolean yesno;
+    yesno=s1.contains("hi");
+    
+    System.out.println(yesno);
+    
+    }
+    
+}
