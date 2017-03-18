215bba095c6a9ff7723c93bdca1cba29b710d299
DELL <DELL@DESKTOP-MC8LGET>
2017-03-18 21:31:41 +0500

    STRING REPLACE

---
+++ JavaApplication8/src/Class9.java
@@ -0,0 +1,17 @@
+
+public class Class9 {
+    public static void main(String[]args)
+    {
+   
+    
+   
+    String s1="hi am hassan am from bahawalnagar";
+    String s2;
+    s2=s1.replace('a','b');
+    
+    
+    System.out.println(s2);
+
+    }
+    
+}
