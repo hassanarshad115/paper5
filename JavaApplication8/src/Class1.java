c4d998a6f6dfd77b26bca28deb68db8776c2e7f6
DELL <DELL@DESKTOP-MC8LGET>
2017-03-18 21:17:23 +0500

    sum and average

---
+++ JavaApplication8/src/Class1.java
@@ -0,0 +1,20 @@
+
+public class Class1 {
+    public static void main(String []args)
+    {
+    int a=5;
+    int b=10;
+    int sum=0;
+    double avg;
+    sum=a+b;
+    avg=sum/2.0;
+    System.out.println("the sum is"+sum);
+        System.out.println();
+
+    System.out.println("the avg is="+avg);
+    
+    
+    
+    }
+    
+}
