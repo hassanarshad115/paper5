2c68ba6770cceee23992ec8b9d8d1d34eecf2ae0
DELL <DELL@DESKTOP-MC8LGET>
2017-03-18 21:22:35 +0500

    string concatenate

---
+++ JavaApplication8/src/Class3.java
@@ -0,0 +1,14 @@
+
+public class Class3 {
+    public static void main(String[]args)
+    {
+    String a="HASSAN";
+    String b="\tARSHAD";
+    String c;
+    c=a+b;
+    System.out.println("the result is\t"+c);
+    
+    
+    }
+    
+}
